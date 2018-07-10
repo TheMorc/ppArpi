de150dd196d49fe3c267e7b42c583655b2eb3dc5
Morc <r.gracik@gmail.com>
2018-07-10 15:45:22 +0200

    Arpi dostal umelý rozum.

--- ppArpi/Program.cs
+++ ppArpi/Program.cs
@@ -15,15 +15,27 @@ using System.Linq;
 
 namespace Morbot
 {
+    public struct Hláška
+    {
+        public Hláška(string hláška, string trigger)
+        {
+            Ahláška = hláška;
+            Atrigger = trigger;
+        }
+
+        public string Ahláška { get; private set; }
+        public string Atrigger { get; private set; }
+    }
+
     class Program
     {
         public static DiscordClient discord;
 
         static VoiceNextExtension voice;
         public static ConfigJSON configuration = new ConfigJSON();
-        public static string version = "1.1.3";
+        public static string version = "Arpiho1.1.3";
         public static DiscordMember owner;
-        public static List<String> arpášoveHlášky = new List<string>() { };
+        public static List<Hláška> arpášoveHlášky = new List<Hláška>() { };
 
         public class ConfigJSON
         {
@@ -46,15 +58,15 @@ namespace Morbot
 
         private static void initArpi()
         {
-            arpášoveHlášky.Add("**Kupujte len kvalitné LED žiarovky PHILIPS®™!!!**");
-            arpášoveHlášky.Add("**No poď mój k tabuli!!!**");
-            arpášoveHlášky.Add("**Napíš tam hore na tabuľu päť!!!**");
-            arpášoveHlášky.Add("**TDA je najhorší zosilňovač ktorý vymyslela firma PHILIPS®™!!!**");
-            arpášoveHlášky.Add("**Hnusný Kapitalizmus chlapci!!!**");
-            arpášoveHlášky.Add("**Hodina začne, keď ja poviem že je koniec!**");
-            arpášoveHlášky.Add("**Predavač nevie čo chceš keď ti rozumie.**");
-            arpášoveHlášky.Add("**Alkalické batérie majú nižšiu kapacitu než tie alkalické.**");
-            arpášoveHlášky.Add("**Vyvolal si sa sám.**");
+            arpášoveHlášky.Add(new Hláška("**Kupujte len kvalitné LED žiarovky PHILIPS®™!!!**", "žiarovka"));
+            arpášoveHlášky.Add(new Hláška("**No poď mój k tabuli!!!**", "arpi"));
+            arpášoveHlášky.Add(new Hláška("**Napíš tam hore na tabuľu päť!!!**", "tda"));
+            arpášoveHlášky.Add(new Hláška("**TDA je najhorší zosilňovač ktorý vymyslela firma PHILIPS®™!!!**", "najlepší"));
+            arpášoveHlášky.Add(new Hláška("**Hnusný Kapitalizmus chlapci!!!**", "dobrý kapitalizmus"));
+            arpášoveHlášky.Add(new Hláška("**Hodina začne, keď ja poviem že je koniec!**", "končí hodina"));
+            arpášoveHlášky.Add(new Hláška("**Predavač nevie čo chceš keď ti rozumie.**", "predavač"));
+            arpášoveHlášky.Add(new Hláška("**Alkalické batérie majú nižšiu kapacitu než tie alkalické.**", "baterky"));
+            arpášoveHlášky.Add(new Hláška("**Vyvolal si sa sám.**", "ja nejdem"));
             //arpášoveHlášky.Add("****");
             //arpášoveHlášky.Add("****");
             //arpášoveHlášky.Add("****");
@@ -88,10 +100,6 @@ namespace Morbot
                 UseInternalLogHandler = true
             });
 
-            voice = discord.UseVoiceNext(new VoiceNextConfiguration
-            {
-                VoiceApplication = DSharpPlus.VoiceNext.Codec.VoiceApplication.LowLatency
-            });
 
             discord.ClientErrored += async e =>
             {
@@ -102,38 +110,55 @@ namespace Morbot
             {
                 if (!e.Author.IsBot)
                 {
-                    if (!e.Message.Content.StartsWith("."))
+                    Random rnd = new Random();
+                    if (e.Message.Content.StartsWith("arpi_invite"))
                     {
-                        Random rnd = new Random();
-                        await discord.SendMessageAsync(e.Channel, arpášoveHlášky[rnd.Next(arpášoveHlášky.Count)]);
+                        await discord.SendMessageAsync(e.Channel, "***No poď mój k odkazu:*** " + "https://discordapp.com/api/oauth2/authorize?client_id=" + discord.CurrentApplication.Id + "&scope=bot");
                     }
-                }
-            };
-            discord.Ready += async e =>
-                {
-                    DiscordActivity game = new DiscordActivity
-                    {
-                        Name = "No poď mój k tabuli",
-                        ActivityType = ActivityType.Playing
-                    };
-                    await discord.UpdateStatusAsync(game);
-
-                };
-            await discord.ConnectAsync();
-            discord.GuildAvailable += async e =>
+                    else if (!e.Message.Content.StartsWith("."))
                     {
 
-                        await e.Guild.GetDefaultChannel().SendMessageAsync("**Zdravím vás, sadnite si.**");
-                        foreach (DiscordMember member in e.Guild.Members)
+                        for (int i = 1; i < arpášoveHlášky.Count; i++)
                         {
-                            if (member.Id == e.Client.CurrentApplication.Owner.Id)
+                            if (e.Message.Content.ToLower().Contains(arpášoveHlášky[i].Atrigger))
                             {
-                                owner = member;
-                                await owner.SendMessageAsync("Arpi beží! ©Madagaskar");
+                                await discord.SendMessageAsync(e.Channel, arpášoveHlášky[i].Ahláška);
+                                return;
                             }
                         }
 
+
+                        await discord.SendMessageAsync(e.Channel, arpášoveHlášky[rnd.Next(arpášoveHlášky.Count)].Ahláška);
+                    }
+
+                }
+            };
+
+            discord.Ready += async e =>
+                    {
+                        DiscordActivity game = new DiscordActivity
+                        {
+                            Name = "No poď mój k tabuli",
+                            ActivityType = ActivityType.Playing
+                        };
+                        await discord.UpdateStatusAsync(game);
+
                     };
+            await discord.ConnectAsync();
+            discord.GuildAvailable += async e =>
+                            {
+
+                                await e.Guild.GetDefaultChannel().SendMessageAsync("**Zdravím vás, sadnite si.**");
+                                foreach (DiscordMember member in e.Guild.Members)
+                                {
+                                    if (member.Id == e.Client.CurrentApplication.Owner.Id)
+                                    {
+                                        owner = member;
+                                        await owner.SendMessageAsync("Arpi beží! ©Madagaskar");
+                                    }
+                                }
+
+                            };
             await Task.Delay(-1);
 
         }
